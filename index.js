2ff5a03be4ed922b90fadfafa31094971a9a24f8
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-11 10:54:48 +0300

    The code is producing the desired output but the tests are failing

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+// Write your solution in this file!
+var customerName='bob';
+console.log(customerName)
+
+const upperCaseCustomerName=()=>
+{
+    return customerName.toUpperCase()
+}
+console.log(upperCaseCustomerName())
+
+const setBestCustomer=()=>
+{
+    bestCustomer=`not ${customerName}`
+}
+setBestCustomer()
+console.log(bestCustomer)
+
+const overwriteBestCustomer=()=>
+{
+    bestCustomer=`maybe ${customerName}`
+}
+overwriteBestCustomer()
+console.log(bestCustomer)
+
+const leastFavoriteCustomer="Simon"
+
+const changeLeastFavoriteCustomer=()=>
+{
+    return leastFavoriteCustomer="Samuel"
+}
+console.log(changeLeastFavoriteCustomer())
